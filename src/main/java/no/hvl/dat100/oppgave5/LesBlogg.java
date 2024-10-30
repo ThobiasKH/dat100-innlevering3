981e713323a7b9f4b904dd5802c0056b9758ec70
ThobiasKH <thobiashoivik@gmail.com>
2024-10-30 13:01:03 +0100

    Clean up

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -15,69 +15,70 @@ import no.hvl.dat100.oppgave3.*;
 
 import javax.swing.JOptionPane;
 
-// meget lite fornøgd med denne
 public class LesBlogg {
 
-	private static String TEKST = "TEKST";
-	private static String BILDE = "BILDE";
+	private static final String TEKST = "TEKST";
+	private static final String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
-        try {
-            // Dette er for unix baserte system. 
-            // tror windows er noe sånn her File file = new File(mappe + "\" + filenavn);
-            // eller new File(mappe + "\\" + filnavn)
-            File file = new File(mappe + "/" + filnavn);
+        //Dette er for linux. 
+        //tror windows er noe sånn her File file = new File(mappe + "\" + filenavn);
+        //eller new File(mappe + "\\" + filnavn)
+        File file = new File(mappe + "/" + filnavn);
 
-            Scanner scanner = new Scanner(file);
+		try (Scanner scanner = new Scanner(file)) {
+			int numInnlegg = Integer.parseInt(scanner.nextLine());
+			Blogg blogg = new Blogg(numInnlegg);
 
-            int numInnlegg = Integer.parseInt(scanner.nextLine());
-            Blogg blogg = new Blogg(numInnlegg);
+			for (int i = 0; i < numInnlegg; i++) {
+				if (!scanner.hasNextLine()) throw new IOException("Filleser sluttet uforventet");
 
-            // her stoler vi veldig på at formatet holder seg, men jaja
-            for (int i = 0; i < numInnlegg; i++) {
-                int numAttributes;
-                if (!scanner.hasNextLine()) throw new IOException();
-                String type = scanner.nextLine();
-                // disgusting stuff right here
-                int failSafe = 0;
-                String failSafeStr = "allGood";
-                if (type.equals("TEKST")) {
-                    blogg.leggTil(
-                        new Tekst(
-                            scanner.hasNextLine() ? Integer.parseInt(scanner.nextLine()) : failSafe++,
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL"),
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL"),
-                            scanner.hasNextLine() ? Integer.parseInt(scanner.nextLine()) : failSafe++,
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL")
-                        )
-                    );
-                    continue;
-                } 
-                if (type.equals("BILDE")) {
-                        blogg.leggTil(
-                        new Bilde(
-                            scanner.hasNextLine() ? Integer.parseInt(scanner.nextLine()) : failSafe++,
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL"),
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL"),
-                            scanner.hasNextLine() ? Integer.parseInt(scanner.nextLine()) : failSafe++,
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL"),
-                            scanner.hasNextLine() ? scanner.nextLine() : (failSafeStr += "FAIL")
-                        )
-                    );
-                    continue;
-                }
-                
-                if (failSafe != 0 || !failSafeStr.equals("allGood")) throw new IOException();
-                
-            } 
-            return blogg;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+				String type = scanner.nextLine();
+				if (TEKST.equals(type)) {
+					blogg.leggTil(parseTekst(scanner));
 
+				} else if (BILDE.equals(type)) {
+					blogg.leggTil(parseBilde(scanner));
+
+				} else {
+					throw new IOException("Ukjent innleggstype: " + type);
+				}
+			}
+			return blogg;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static Tekst parseTekst(Scanner scanner) throws IOException {
+		try {
+			int id = Integer.parseInt(scanner.nextLine());
+			String bruker = scanner.nextLine();
+			String dato = scanner.nextLine();
+			int likes = Integer.parseInt(scanner.nextLine());
+			String tekst = scanner.nextLine();
+			return new Tekst(id, bruker, dato, likes, tekst);
+		} catch (Exception e) {
+			throw new IOException("Kan ikke lese inn data fra TEKST innlegg", e);
+		}
+	}
+
+	private static Bilde parseBilde(Scanner scanner) throws IOException {
+		try {
+			int id = Integer.parseInt(scanner.nextLine());
+			String bruker = scanner.nextLine();
+			String dato = scanner.nextLine();
+			int likes = Integer.parseInt(scanner.nextLine());
+			String tekst = scanner.nextLine();
+			String url = scanner.nextLine();
+			return new Bilde(id, bruker, dato, likes, tekst, url);
+		} catch (Exception e) {
+			throw new IOException("Kan ikke lese inn data fra BILDE innlegg", e);
+		}
 	}
 }
